60d7c09b1bee9a6526a58862a1318bdb9bc3f171
Alain Martel <alain74martel@gmail.com>
2017-11-16 14:00:25 -0500

    Update VolUtility_Install.sh

--- VolUtility_Install.sh
+++ VolUtility_Install.sh
@@ -1,16 +1,18 @@
 cd ~
 #Ubuntu 16.04 LTS
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
-sudo apt-get -y install automake bison build-essential docker flex gcc git libjpeg8 libjpeg8-dev libmagic-dev libpcre++-dev libssl-dev libtool make pcregrep python-dev python-lxml python-pip python-socksipy python-urllib3 python-yara unzip 
+sudo apt-get -y install autoconf automake autopoint bison build-essential curl docker flex gcc gettext git libimage-exiftool-perl libjpeg8 libjpeg8-dev libmagic-dev libpcre++-dev libssl-dev libtool make nano pcregrep pkg-config python-dev python-lxml python-pip python-pip python-socksipy python-urllib3 python-yara unzip 
 sudo -H pip install --upgrade pip
 
-#MongoDB Docker Install
-#sudo docker run -d -p 27017:27017 --name vol-mongo mongo
+#MongoDB Install
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo service mongod start
+
+mkdir git
+cd git
 
 #Yara Install
 sudo -H pip install distorm3 pycrypto pillow ujson pyinstrument haystack pycoin simplejson pefile dpapick pysocks requests construct==2.5.5-reupload
@@ -23,6 +25,31 @@ sudo make install
 sudo make check
 cd ..
 
+# Install SSDEEP
+ curl -sSL http://sourceforge.net/projects/ssdeep/files/ssdeep-2.13/ssdeep-2.13.tar.gz/download | tar -xzC . 
+ cd ssdeep-2.13 
+ ./configure
+ make 
+ make install 
+ pip install pydeep 
+ cd .. 
+ 
+ # Get the maxmind database for ip lookup
+ curl -sSL http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz > GeoLite2-City.mmdb.gz 
+ gzip -d GeoLite2-City.mmdb.gz 
+ mkdir /usr/share/GeoIP 
+ mv GeoLite2-City.mmdb /usr/share/GeoIP/
+ 
+ # Install and Build libpff
+ git clone https://github.com/libyal/libpff.git 
+ cd libpff 
+ ./synclibs.sh 
+ ./autogen.sh 
+ ./configure --enable-python 
+ make 
+ make install 
+ ldconfig
+ 
 #VirusTotal-api
 sudo pip install virustotal-api
 
@@ -39,3 +66,6 @@ git clone https://github.com/AJMartel/VolUtility
 cd VolUtility
 sudo pip install -r requirements.txt
 sudo nohup python manage.py runserver 0.0.0.0:8000 &
+
+# Clean Up
+ rm -rf ~/git
